3fd4b8f1dbf83fad38b411f682527f327c299b76
nullday <aseaday@hotmail.com>
2022-12-18 00:42:35 +0800

    Add demo.py

--- demo.py
+++ demo.py
@@ -1,27 +1,10 @@
-from backtesting import Backtest, Strategy
-from backtesting.lib import crossover
-
+from backtesting import Backtest, StrategyWithContext
 from backtesting.test import GOOG
-from backtesting.tlib import SMA, RSI
-import pandas as pd
-from dataclasses import dataclass, field, fields
 
 
-class SmaCross(Strategy):
-    def init(self):
-        self.ma1 = self.I(SMA, self.data, 10)
-        self.ma2 = self.I(SMA, self.data, 20)
-        self.rsi14 = self.I(RSI,self.data, 14)
-        print(self.rsi14)
-
-    def next(self):
-        if crossover(self.ma1, self.ma2):
-            self.buy()
-        elif crossover(self.ma2, self.ma1):
-            self.sell()
-
-
-bt = Backtest(GOOG, SmaCross, commission=.002,
-              exclusive_orders=True)
-stats = bt.run()
+bt = Backtest(GOOG, StrategyWithContext, exclusive_orders=True)
+stats = bt.run(udf={
+    "buy_udf": "RSI(14) > 70",
+    "sell_udf": "RSI(14) < 30"
+})
 print(stats)
